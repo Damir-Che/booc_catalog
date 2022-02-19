4b8868942aabc056c0e8fc5c2ad6550aac84f46a
Damir <chuchehov@gmail.com>
2022-02-19 16:26:25 +0300

    added CRUD and views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,48 @@
 # frozen_string_literal: true
 
 class BooksController < ApplicationController
+
+  before_action :set_books, only: [:edit, :update, :destroy]
+
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(books_params)
+    if @book.save
+      redirect_to books_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @book.update_attributes(books_params)
+      redirect_to books_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_path, success: 'Статья успешно удалена'
+  end
+
+  private
+
+  def books_params
+    params.require(:book).permit(:name, :author)
+  end
+
+  def set_books
+    @book = Book.find(params[:id])
+  end
 end
