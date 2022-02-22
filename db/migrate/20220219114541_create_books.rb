0947b0dcd6c8408dd54ed15e30169f87a43093ae
Damir <chuchehov@gmail.com>
2022-02-22 15:02:31 +0300

    Added search by dates

--- db/migrate/20220219114541_create_books.rb
+++ db/migrate/20220219114541_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration[5.2]
     create_table :books do |t|
       t.string :name
       t.string :author
-      t.string :date
+      t.integer :date
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2022_02_19_114541) do
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "name"
     t.string "author"
-    t.string "date"
+    t.integer "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
